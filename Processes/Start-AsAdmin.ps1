125f9e8fd297877c1377b5f52aed7c108f38d686
Jim Roton <jim.roton@live.com>
2023-07-19 17:38:10 -0500

    updated process

--- Processes/Start-AsAdmin.ps1
+++ Processes/Start-AsAdmin.ps1
@@ -1,12 +1,12 @@
 # ---------------------------------------------------
 # -  Author: Jim Roton
 # -    Date: 2023.07.18
-# -    Desc: Run any process with elevated privilages. Defaults to running a new
+# -    Desc: Run any process with elevated privilages (Windows only). Defaults to running a new
 #       PowerShell window as administrator.
 #
 # - Example: Start-AsAdmin
 # - Example: Start-AsAdmin -FilePath "cmd.exe"
-# - Example: Start-AsAdmin -FilePath ""
+# - Example: Start-AsAdmin -FilePath "pwsh.exe" -Arguments "-noexit", "-command ""cmd.exe /C help"""
 # ---------------------------------------------------
 param(
     [Parameter(
@@ -14,7 +14,7 @@ param(
         Mandatory       = $False,
         HelpMessage     = "Process to run as administrator."
     )]
-    [String] $FilePath  = "pwsh",
+    [String] $FilePath  = "pwsh.exe",
     [Parameter(
         Position        = 1,
         Mandatory       = $False,
@@ -24,8 +24,15 @@ param(
 )
 
 try {
-    Start-Process -FilePath $FilePath -ArgumentList $Arguments -Verb RunAs -ErrorAction Stop;
 
+    # use runas on windows
+    if ($IsWindows){
+        Start-Process -FilePath $FilePath -ArgumentList $Arguments -Verb RunAs -ErrorAction Stop;
+
+    # use sudo on mac and linux
+    } else {
+        Start-Process -FilePath "sudo" -ArgumentList (@($FilePath) + @($Arguments));
+    }
 } catch {
     Write-Host -ForegroundColor Red $_;
 }
