379f75340dca17310fa6efdffeac54a3fcd9c616
Jim Roton <jim.roton@live.com>
2023-07-20 22:25:02 -0500

    updated gitadd-items

--- Items/GitAdd-Items.ps1
+++ Items/GitAdd-Items.ps1
@@ -9,32 +9,38 @@ param(
     [String[]] $ItemList,
     [Parameter(
         Position            = 0,
-        Mandatory           = $True,
+        Mandatory           = $False,
         HelpMessage         = "Path to search for items.",
         ParameterSetName    = "ItemPath"
     )]
-    [String] $Path,
+    [String] $Path          = ".",
     [Parameter(
         Position            = 0,
         Mandatory           = $False,
         HelpMessage         = "Filter for selecting items.",
         ParameterSetName    = "ItemPath"
     )]
-    [String] $Filter
+    [String] $Pattern        = ""
 )
 
 try {
 
     # get item list by filter
     if ($null -eq $ItemList){
-        $ItemList = Start-Process -FilePath "git" -ArgumentList "status", "-s" -ErrorAction Stop; #Get-ChildItem -Recurse -Filter $Filter -ErrorAction Stop;  
+#        $ItemList = Start-Process -FilePath "git" -ArgumentList "status", "-s" -WorkingDirectory $Path -PassThru -ErrorAction Stop;
+        $FileList = (git $FilePath status -s) -Match "^\?";
+$FileList;        
+        # foreach ($_ in $FileList){
+        #     ($_ -Split " ")[2];
+        #     #            $ItemList.Add($_.Split(" ")[1]);
+        # }
     }
 
-    if ($null -ne $ItemList){
-        $ItemList | Start-Process -FilePath "git" -ArgumentList {"-add", $_};
-    }
+    # if ($null -ne $ItemList){
+    #     $ItemList | Start-Process -FilePath "git" -ArgumentList {"-add", $_};
+    # }
 
-    $ItemList;
+#    $ItemList;
 } catch {
     Write-Host -ForegroundColor Red $_;
 }
